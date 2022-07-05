0f880e62feaddd433abf8e245e6f4a1cbec0b5a0
danomisik <misik.fiit@gmail.com>
2022-07-05 16:27:05 +0200

    Add test enviroment for database

--- src/database.ts
+++ src/database.ts
@@ -3,16 +3,26 @@ import { Pool } from 'pg';
 
 dotenv.config();
 
-const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, ENV } =
+const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_TEST_DB, ENV } =
   process.env;
 
 console.log(`ENV started: ${ENV}`);
+let client: Pool;
 
-const client = new Pool({
+if(ENV === 'test') {
+  client = new Pool({
+  host: POSTGRES_HOST,
+  database: POSTGRES_TEST_DB,
+  user: POSTGRES_USER,
+  password: POSTGRES_PASSWORD
+});
+} else {
+  client = new Pool({
   host: POSTGRES_HOST,
   database: POSTGRES_DB,
   user: POSTGRES_USER,
   password: POSTGRES_PASSWORD
 });
+}
 
 export default client;
